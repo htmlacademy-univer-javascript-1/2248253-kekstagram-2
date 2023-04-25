94d669cb92c6c71ec9fbbf6a938b6ddc5334c9c2
KoCHeVa-KaRIna <123810434+KoCHeVa-KaRIna@users.noreply.github.com>
2023-04-25 17:57:27 +0500

    Create util.js

---
+++ js/util.js
@@ -0,0 +1,16 @@
+export function getRandomInt(from, to) {
+  if (from < 0 || to < 0) {
+    throw new RangeError('Числа в диапазоне должны быть положительными');
+  }
+
+  if (from === to) {
+    return from;
+  }
+  if (from > to) {
+    [from, to] = [to, from];
+  }
+
+  return Math.round(Math.random() * (to - from) + from);
+}
+
+export const isCorrectLength = (str, maxLength) => str.length <= maxLength;
