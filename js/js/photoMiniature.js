9ef34eaf46fdb0a23a9fb54df1302dec2ef150f7
KoCHeVa-KaRIna <123810434+KoCHeVa-KaRIna@users.noreply.github.com>
2023-04-26 11:24:45 +0500

    Update photoMiniature.js

--- js/js/photoMiniature.js
+++ js/js/photoMiniature.js
@@ -1,15 +1,24 @@
+import {showBigPicture} from './photoFull.js';
+
 const pictureTemplate = document.querySelector('#picture');
 const pictureContainer = document.querySelector('.pictures');
 
 export function insertPhotoMiniature(descriptions) {
   const pictureFragment = document.createDocumentFragment();
-  for (const {url, likes, comments} of descriptions) {
+  for (const desc of descriptions) {
     const picture = pictureTemplate.cloneNode(true).content;
-    picture.querySelector('.picture__img').src = url;
-    picture.querySelector('.picture__likes').textContent = likes;
-    picture.querySelector('.picture__comments').textContent = comments.length;
+    picture.querySelector('.picture__img').src = desc.url;
+    picture.querySelector('.picture').addEventListener('click', (ev) => {
+      ev.preventDefault();
+      showBigPicture(desc);
+    });
+    picture.querySelector('.picture__likes').textContent = desc.likes;
+    picture.querySelector('.picture__comments').textContent = desc.comments.length;
     pictureFragment.appendChild(picture);
   }
-
   pictureContainer.appendChild(pictureFragment);
+}
+
+export function clearPhotoMiniature(){
+  pictureContainer.querySelectorAll('.picture').forEach((p) => p.remove());
 }
